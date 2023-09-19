a1ac63357dfba1c5868c1b181d5b740a722140dd
jtpAPC2020 <79078787+jtpAPC2020@users.noreply.github.com>
2023-09-19 16:34:28 +0800

    final version of system after scsproj

---
+++ ADENICSY2/admin/includes/sidebar.php
@@ -0,0 +1,35 @@
+  <div id="layoutSidenav_nav">
+      <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion" style="background-color:#583c7c;">
+          <div class="sb-sidenav-menu">
+              <div class="nav">
+                  <div class="sb-sidenav-menu-heading">Core</div>
+                  <a class="nav-link" href="dashboard.php">
+                      <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                      Dashboard
+                  </a>
+
+
+                  <a class="nav-link" href="manage-users.php">
+                      <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                      Manage Patient Records
+                  </a>
+
+                  <a class="nav-link" href="manage-employee.php">
+                      <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                      Manage Employee Records
+                  </a>
+
+                  <a class="nav-link" href="bwdates-report-ds.php">
+                      <div class="sb-nav-link-icon"><i class="fa fa-calendar"></i></div>
+                      Search Patient on Registered Date
+                  </a>
+
+                  <a class="nav-link" href="logout.php">
+                      <div class="sb-nav-link-icon"><i class="fas fa-sign-out-alt"></i></div>
+                      Signout
+                  </a>
+              </div>
+          </div>
+
+      </nav>
+  </div>
