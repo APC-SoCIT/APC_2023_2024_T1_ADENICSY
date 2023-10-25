b40994ad5af8f0f958b530f8c14969e3a7cfed8f
jtpAPC2020 <79078787+jtpAPC2020@users.noreply.github.com>
2023-10-25 15:29:39 +0800

    change db name

--- ADENICSY2.1/includes/config.php
+++ ADENICSY2.1/includes/config.php
@@ -2,9 +2,9 @@
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'loginsystem');
+define('DB_NAME', 'adenicsy');
 $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-$con = mysqli_connect('localhost', 'root', '', 'loginsystem');
+$con = mysqli_connect('localhost', 'root', '', 'adenicsy');
 date_default_timezone_set('Asia/Manila');
 
 // Check connection
