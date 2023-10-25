70a44cc16b14d416b4c91d28a4b1555e37ea56bc
jtpAPC2020 <79078787+jtpAPC2020@users.noreply.github.com>
2023-10-25 16:33:12 +0800

    changing credentials

--- ADENICSY2.1/includes/config.php
+++ ADENICSY2.1/includes/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'adenicsy');
-define('DB_PASS', 'adenicsy');
+define('DB_PASS', '');
 define('DB_NAME', 'adenicsy');
 $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 $con = mysqli_connect('localhost', 'adenicsy', 'adenicsy', 'adenicsy');
