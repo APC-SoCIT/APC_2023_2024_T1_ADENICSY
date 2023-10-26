c837c4200c6fdc48708b448cd6b63e4900a8d7dc
jtpAPC2020 <79078787+jtpAPC2020@users.noreply.github.com>
2023-10-26 13:58:05 +0800

    updated config inside patiet

--- ADENICSY2.1/patient/includes/config.php
+++ ADENICSY2.1/patient/includes/config.php
@@ -2,11 +2,11 @@
 define('EMAIL', 'apelodentalclinicsystem@gmail.com');
 define('PASSWORD', 'xcafbqiigrvxfirv');
 define('DB_SERVER', 'localhost');
-define('DB_USER', 'root');
+define('DB_USER', 'adenicsy');
 define('DB_PASS', '');
-define('DB_NAME', 'loginsystem');
+define('DB_NAME', 'adenicsy');
 $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-$con = mysqli_connect('localhost', 'root', '', 'loginsystem');
+$con = mysqli_connect('localhost', 'adenicsy', '', 'adenicsy');
 date_default_timezone_set('Asia/Manila');
 
 // Check connection
