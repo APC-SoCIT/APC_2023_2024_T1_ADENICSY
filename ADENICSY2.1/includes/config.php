80f65d477a9952ec5b19571a07e7ff384dfa88cf
jtpAPC2020 <79078787+jtpAPC2020@users.noreply.github.com>
2023-10-26 14:55:18 +0800

    updated 2 config files

--- ADENICSY2.1/includes/config.php
+++ ADENICSY2.1/includes/config.php
@@ -1,10 +1,10 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'adenicsy');
-define('DB_PASS', '');
+define('DB_PASS', 'adenicsy');
 define('DB_NAME', 'adenicsy');
 $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-$con = mysqli_connect('localhost', 'adenicsy', '', 'adenicsy');
+$con = mysqli_connect('localhost', 'adenicsy', 'adenicsy', 'adenicsy');
 date_default_timezone_set('Asia/Manila');
 
 // Check connection
